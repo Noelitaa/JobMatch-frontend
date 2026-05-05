3f3fa2e561110aa1e9bf525a1d80c84afe5fea8e
Noelitaa <noeliafallasbermudez@gmail.com>
2026-05-04 23:03:21 -0600

    delete

--- app/src/main/java/com/moviles/jobmatch/data/CompanyMap.kt
+++ app/src/main/java/com/moviles/jobmatch/data/CompanyMap.kt
@@ -1,7 +1,7 @@
 package com.moviles.jobmatch.data
 
 object CompanyMap {
-    // Mapa de nombre de empresa -> ID
+    // Mapa de nombre de empresa ID
     val companies = mapOf(
         "Tech Solutions" to "D3888166-1643-435E-BC10-347D8DEB285C",
         "Nueva Tech Solutions" to "FE4CA8C7-649F-4677-1E7B-08DEA6254CF1"
